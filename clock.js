31d16e2cf7e54c925b49db3fe106345a7d2dafaf
Kundan <ntamsoy662gmail.com>
2023-02-19 15:20:40 +0530

    Added effects

--- clock.js
+++ clock.js
@@ -21,10 +21,17 @@ if (second < 10 ){
 }
 if (am > 12){
     document.getElementById('ampm').innerHTML='PM';
-    // console.log("working");
-    // console.log(am);
 }
+hh = document.getElementById('hh');
+mm = document.getElementById('mm');
+ss = document.getElementById('ss');
+
 document.getElementById('hours').innerHTML=hours;
 document.getElementById('minute').innerHTML=minute;
 document.getElementById('second').innerHTML=second;
+
+hh.style.strokeDashoffset = 440 - (440 * hours) / 12;
+mm.style.strokeDashoffset = 440 - (440 * minute) / 60;
+ss.style.strokeDashoffset = 440 - (440 * second) / 60;
+
 }, 1000);
